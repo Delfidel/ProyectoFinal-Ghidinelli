84bd0b7859641d78cffa48dffbe8c5896c52c655
Delfina Ghidinelli <delfinaghidinelli@gmail.com>
2022-12-12 16:03:24 -0300

    Agregué array

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 
+
+
 /**IF condicional */
 let edad = parseInt(prompt("Ingrese su edad:"))
 
@@ -60,15 +62,84 @@ holis()
 
 
 
+//**array o arreglo */
+
+//*let arrayHabitaciones = ["King", "Doble", "Triple" ];console.log("Elija el tamaño de su pieza deseada: 1 = King, 2 = Doble, 3 = Triple");console.log(arrayHabitaciones);
+
+//*let arraySemana = [1, 2, 3 ];
+/**console.log("Por Favor, elija el tiempo que va a pasar en nuestra estancia.");
+/**console.log(arraySemana);**/
+
+
+class reserva {
+    constructor(pieza, tamaño, precio) {
+        this.pieza = pieza;
+        this.tamaño= tamaño;
+        this.precio = precio;
+    }
+}
+
+const reservaHabi1 = new reserva("Classic", "King", 5000);
+const reservaHabi2 = new reserva("Classic", "Doble", 8000);
+const reservaHabi3 = new reserva("Classic", "Triple", 10000);
+const reservaHabi4 = new reserva("Premium", "King", 15000);
+const reservaHabi5 = new reserva("Premium", "Doble", 17000);
+const reservaHabi6 = new reserva("Premium", "Triple", 20000);
+
+const arrayCliente = [];
+
+arrayCliente.push(reservaHabi1);
+arrayCliente.push(reservaHabi2);
+arrayCliente.push(reservaHabi3);
+arrayCliente.push(reservaHabi4);
+arrayCliente.push(reservaHabi5);
+arrayCliente.push(reservaHabi6);
+
+console.log(arrayCliente);
 
 
 
 
+function menu() {
+    alert("Ingrese el tamaño preferido de su habitacion!");
+    let opcion = parseInt(prompt("Ingrese una opción: \n 1) King\n 2) Doble \n 3) Triple \n "));
+    return opcion;
+}
+
+function altaReserva() {
+    let nombre = prompt("Ingrese su nombre: ");
+    let apellido = prompt("Ingrese su apellido: ");
+    let dni = parseInt(prompt("Ingrese su DNI: "));
+    let cliente = new Huesped(nombre, apellido, dni, habitacion);
+    arrayClientes.push(huesped);
+    console.log(arrayCliente);
+}
+function bajaReserva() {
+    let dni = parseInt(prompt("Ingrese su DNI: "));
+    let cliente = arrayReserva.find(huesped => huesped.dni === dni);
+    let indice = arrayReserva.indexOf(huesped);
+    arrayClientes.splice(indice, 1);
+    console.log(arrayCliente);
+}
 
 
+function salir() {
+    alert("Gracias por reservar en Baok!");
+}
 
 
-
-
-
-
+let opcion = menu();
+switch (opcion) {
+    case 1:
+        altaReserva();
+        break;
+    case 2:
+        bajaReserva();
+        break;
+    case 3:
+        salir();
+        break;
+    default:
+        alert("Opción incorrecta!");
+        break;
+}
