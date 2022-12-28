8d07b893c0fc0894f9ce85316dd03c28793a5f22
Delfina Ghidinelli <delfinaghidinelli@gmail.com>
2022-12-28 18:27:08 -0300

    cambios java

--- main.js
+++ main.js
@@ -1,60 +1,64 @@
-
-
-
 class Reservo {
     constructor(nombreCliente, apellidoCliente, numeroReserva, habitacion) {
-    this.nombreCliente = nombreCliente;
-    this.apellidoCliente = apellidoCliente;
-    this.numeroReserva = numeroReserva;
-    this.habitacion = habitacion;
+      this.nombreCliente = nombreCliente;
+      this.apellidoCliente = apellidoCliente;
+      this.numeroReserva = numeroReserva;
+      this.habitacion = habitacion;
     }
-}
-
-const reservas = [];
-/*****************************/
+  }
+  const nombreCliente = [];
+  const apellidoCliente = [];
+  const numeroReserva = [];
+  const habitacion = [];
+  const newReservo = [];
+  const nuevaReserva = newReservo (nombreCliente, apellidoCliente, numeroReserva, habitacion);
+  
+  
+  /*****************************/
   //Si el LocalStorage tiene datos, los agrego al Array de Reservas.
-
-if (localStorage.getItem('reservas')) {
-    let reserva = JSON.parse(localStorage.getItem('reservas'));
+  
+  if (localStorage.getItem('reservo')) {
+    let reserva = JSON.parse(localStorage.getItem('reserva'));
     /* reservas.push(...reserva); */
     for (let i = 0; i < reserva.length; i++) {
-    reservas.push(reserva[i]);
+      reservas.push(reserva[i]);
     }
-}
-
-/*****************************/
-const formulario = document.getElementById ('formulario');
-
-formulario.addEventListener('submit', (e) => {
+  }
+  
+  /*****************************/
+  
+  const formulario = document.getElementById('formulario');
+  
+  formulario.addEventListener('submit', (e) => {
     e.preventDefault();
     agregarReserva();
-});
-
-function agregarReserva() {
+  });
+  
+  function agregarReserva() {
     const nombre = document.getElementById('nombre').value;
     const apellido = document.getElementById('apellido').value;
     const reserva = document.getElementById('reserva').value;
     const habitacion = document.getElementById('habitacion').value;
-    const nuevaReserva = new Reserva(nombre, apellido, reserva, habitacion);
+    const nuevaReserva = newReservo(nombre, apellido, reserva, habitacion); !!111111
     reservas.push(nuevaReserva);
     //Agrego al LocalStorage:
-    localStorage.setItem('reservas', JSON.stringify(reservas));
+    localStorage.setItem('reserva', JSON.stringify(reservas));
     formulario.reset();
-}
-
-const contenedorReservas = document.getElementById('contenedorReservas');
-
-const verReservas = document.getElementById('verReservas');
-
-verReservas.addEventListener('click', () => {
+  }
+  
+  const contenedorReservas = document.getElementById('contenedorReservas');
+  
+  const verReservas = document.getElementById('verReservas');
+  
+  verReservas.addEventListener('click', () => {
     mostrarReservas();
-});
-
-function mostrarReservas() {
+  });
+  
+  function mostrarReservas() {
     contenedorReservas.innerHTML = '';
     reservas.forEach((reserva) => {
-    const div = document.createElement('div');
-    div.innerHTML = `
+      const div = document.createElement('div');
+      div.innerHTML = `
                         <div>
                             <p>Nombre del Cliente: ${reserva.nombreCliente}</p>
                             <p>Apellido del Cliente: ${reserva.apellidoCliente}</p>
@@ -63,6 +67,7 @@ function mostrarReservas() {
                         </div>
         
                         `;
-    contenedorReservas.appendChild(div);
+      contenedorReservas.appendChild(div);
     });
-}  
+  }
+  
